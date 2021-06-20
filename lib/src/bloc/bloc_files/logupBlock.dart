337a401bafd576b152792f01705599868bce14a8
Fernando Quintanilla <davidfernando110@unitec.edu>
2021-06-19 23:24:39 -0600

    LogUpFuncionando

--- lib/src/bloc/bloc_files/logupBlock.dart
+++ lib/src/bloc/bloc_files/logupBlock.dart
@@ -3,35 +3,56 @@ import 'package:rxdart/rxdart.dart';
 import 'package:hm_help/src/bloc/bloc_validators/validators.dart';
 
 class LogupBloc with Validator {
+  final _emailController = BehaviorSubject<String>();
   final _nombreController = BehaviorSubject<String>();
   final _apellidoController = BehaviorSubject<String>();
-  final _emailController = BehaviorSubject<String>();
   final _passwordController = BehaviorSubject<String>();
+  final _sexoController = BehaviorSubject<String>();
+  final _imagenController = BehaviorSubject<String>();
+  final _fechaController = BehaviorSubject<String>();
+  
 
-  Stream<String> get nombreStream => _emailController.stream;
-  Stream<String> get apellidoStream => _apellidoController.stream;
   Stream<String> get emailStream =>
       _emailController.stream.transform(validarEmail);
+  Stream<String> get nombreStream => _emailController.stream;
+  Stream<String> get apellidoStream => _apellidoController.stream;
   Stream<String> get passwordStream =>
       _passwordController.stream.transform(validarPassword);
+  Stream<String> get sexoStream => _sexoController.stream;
+  Stream<String> get imagenStream => _imagenController.stream;
+  Stream<String> get fechaStream => _fechaController.stream;
+  
 
   Stream<bool> get formValidStream =>
       Rx.combineLatest2(emailStream, passwordStream, (e, p) => true);
 
+  Function(String) get changeEmail => _emailController.sink.add;
   Function(String) get changeNombre => _nombreController.sink.add;
   Function(String) get changeApellido => _apellidoController.sink.add;
-  Function(String) get changeEmail => _emailController.sink.add;
   Function(String) get changePassword => _passwordController.sink.add;
+  Function(String) get changeSexo => _sexoController.sink.add;
+  Function(String) get changeImagen => _imagenController.sink.add;
+  Function(String) get changeFecha => _fechaController.sink.add;
 
+
+  String? get email => _emailController.value;
   String? get nombre => _nombreController.value;
   String? get apellido => _apellidoController.value;
-  String? get email => _emailController.value;
   String? get password => _passwordController.value;
+  String? get sexo => _sexoController.value;
+  String? get imagen => _imagenController.value;
+  String? get fecha => _fechaController.value;
+  
 
   dispose() {
+    _emailController.close();
     _nombreController.close();
     _apellidoController.close();
-    _emailController.close();
     _passwordController.close();
+    _sexoController.close();
+    _imagenController.close();
+    _fechaController.close();
+    
+    
   }
 }
